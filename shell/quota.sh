ca30e2507c49919a28af4ecbb0791b3c2075b52f
labrick <yananback@gmail.com>
2017-02-15 16:51:58 +0800

    加入参数选择

--- shell/quota.sh
+++ shell/quota.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-awk -F: '{if($3>500) print $1>"./tmpfile"}' /etc/passwd
-
-for user in $(cat "./tmpfile")
-do
-    miduser_flag=0
-    for miduser in $(cat "/mnt/hdd2/yanan/.miduserdb")
+quota_init() {
+    awk -F: '{if($3>500) print $1>"./tmpfile"}' /etc/passwd
+    for user in $(cat "./tmpfile")
     do
-        if [ $user = $miduser ]; then
-            miduser_flag=1
+        miduser_flag=0
+        for miduser in $(cat "/mnt/hdd2/yanan/.miduserdb")
+        do
+            if [ $user = $miduser ]; then
+                miduser_flag=1
+            fi
+        done
+    
+        if [ "$miduser_flag" -eq 1 ]; then
+            echo $user is a miduser, no deal with it
+            setquota $user 420000000 500000000 0 0 /mnt/hdd2
+        else
+            echo $user is a general user, deal with it
+            setquota $user 110000000 150000000 0 0 /mnt/hdd2
         fi
     done
+}
 
-    if [ "$miduser_flag" -eq 1 ]; then
-        echo $user is a miduser, no deal with it
-        setquota $user 420000000 500000000 0 0 /mnt/hdd2
-    else
-        echo $user is a general user, deal with it
-        setquota $user 110000000 150000000 0 0 /mnt/hdd2
-    fi
+quota_help() {
+    cat <<EOF
+Usage: `basename $0` replace hexo with parameter "-d" and "-g".
+options:
+    -d display the info
+    -i init the quota
+    -h display this help menu
+EOF
+}
+
+quota_info() {
+    repquota /mnt/hdd2
+}
+
+while getopts di option; do
+    case "$option" in
+        d) quota_info;;
+        i) quota_init;;
+        \?) quota_help
+            exit 1;;
+    esac
 done
 
 rm -rf ./tmpfile
 
-repquota /mnt/hdd2
