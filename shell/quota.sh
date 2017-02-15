c938ddf046f2b765fbaab1572adec1fa1881bc91
labrick <yananback@gmail.com>
2017-02-15 16:07:02 +0800

    增加miduser支持

--- shell/quota.sh
+++ shell/quota.sh
@@ -4,10 +4,19 @@ awk -F: '{if($3>500) print $1>"./tmpfile"}' /etc/passwd
 
 for user in $(cat "./tmpfile")
 do
-    if [ $user = "yanan" ]||[ $user = "zone" ]||[ $user = "ProjectGroup" ]; then
-        echo "no deal with it"
+    miduser_flag=0
+    for miduser in $(cat "/mnt/hdd2/yanan/.miduserdb")
+    do
+        if [ $user = $miduser ]; then
+            miduser_flag=1
+        fi
+    done
+
+    if [ "$miduser_flag" -eq 1 ]; then
+        echo $user is a miduser, no deal with it
+        setquota $user 420000000 500000000 0 0 /mnt/hdd2
     else
-        echo "deal with it"
+        echo $user is a general user, deal with it
         setquota $user 110000000 150000000 0 0 /mnt/hdd2
     fi
 done
