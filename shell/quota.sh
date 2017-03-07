cbe1c557581d130aa8e3212c310d2d47f4017d00
labrick <yananback@gmail.com>
2017-03-07 15:17:41 +0800

    将log日志写入log文件

--- shell/quota.sh
+++ shell/quota.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOGFILE="/mnt/hdd2/yanan/bin/log/quota.log"
+
 quota_init() {
     awk -F: '{if($3>500) print $1>"./tmpfile"}' /etc/passwd
     for user in $(cat "./tmpfile")
@@ -21,13 +23,13 @@ quota_init() {
         done
     
         if [ "$supuser_flag" -eq 1 ]; then
-            echo $user is a super user, deal with it
+            echo `date`: $user is a super user, deal with it >> $LOGFILE
             sudo setquota $user 500000000 520000000 0 0 /mnt/hdd2
         elif [ "$miduser_flag" -eq 1 ]; then
-            echo $user is a miduser, no deal with it
+            echo `date`: $user is a miduser, no deal with it >>$LOGFILE
             sudo setquota $user 300000000 320000000 0 0 /mnt/hdd2
         else
-            echo $user is a general user, deal with it
+            echo `date`: $user is a general user, deal with it >> $LOGFILE
             sudo setquota $user 100000000 120000000 0 0 /mnt/hdd2
         fi
     done
@@ -47,9 +49,14 @@ quota_info() {
     sudo repquota /mnt/hdd2
 }
 
-while getopts di option; do
+if [ $# -eq 0 ]; then
+    quota_init;
+fi
+
+while getopts dhi option; do
     case "$option" in
         d) quota_info;;
+	    h) quota_help;;
         i) quota_init;;
         \?) quota_help
             exit 1;;
@@ -58,3 +65,4 @@ done
 
 rm -rf ./tmpfile
 
+echo ================================================= >> $LOGFILE
